db226065373c79362ffdaa972096c59f675ee175
rajivgupta1 <100814533+rajivgupta1@users.noreply.github.com>
2022-03-03 12:15:11 +1100

    Create switchcase.c

---
+++ switchcase.c
@@ -0,0 +1,45 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+ void main()
+{
+    
+    float num1;
+    float num2;
+    char op;
+    float result;
+    
+    
+    printf("enter the first number:");
+    scanf("%f",&num1);
+    
+     printf("enter the operation:");
+    scanf(" %c",&op);
+    
+     printf("enter the second number:");
+    scanf("%f",&num2);
+    
+    switch(op)
+    {
+    case '-':
+    result=num1-num2;
+    printf("%f",result);
+    break;
+    
+     case '+':
+    result=num1+num2;
+    printf("%f",result);
+    break;
+    
+     case '*':
+    result=num1*num2;
+    printf("%f",result);
+    break;
+     case '/':
+    result=num1/num2;
+    printf("%f",result);
+    break;
+    
+    default:
+    printf("the operator is not valid");
+    }
+}
